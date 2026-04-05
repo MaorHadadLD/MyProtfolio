ab0ad437c6919aecf10992afe504b9a7a6f251c5
Maor <maorhadad94@gmail.com>
2026-04-05 21:20:24 +0300

    update route

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -5,6 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const body = await req.json();
     const { name, email, message } = body;
 
